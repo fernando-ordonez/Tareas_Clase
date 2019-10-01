b3254cd0055efd22e116910aa809ec6dbcbf76b8
fernando-ordonez <55764793+fernando-ordonez@users.noreply.github.com>
2019-10-01 14:33:10 -0500

    Update Area Circulo.c

--- Area Circulo.c
+++ Area Circulo.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 #define pi 3.1416
 
 int main()
@@ -6,7 +7,8 @@ int main()
     float radio,area=0;
     printf("Ingrese el radio");
     scanf("%f",&radio);
-    area=pi*radio*radio;
+    //area=pi*radio*radio;
+    area=pi*pow(radio,6);
     printf("El area es %f.", area);
     return 0;
 }
